7b085311e79e5557497fe4a25732f5d3a3feb192
abydityadev <abyan21@smpia9.sch.id>
2023-07-05 17:35:32 +0700

    ✨

--- src/app/tasbeeh/page.jsx
+++ src/app/tasbeeh/page.jsx
@@ -5,17 +5,17 @@ import React, { useState } from 'react'
 import { motion } from 'framer-motion'
 
 export default function page() {
-    const [tasbeeh, setTasbeeh] = useState(0)
+    const [Tasbeeh, setTasbeeh] = useState(0)
 
     return (
         <section className='text-center mt-11'>
             <h1 className='text-4xl text-gray-300 font-bold'>
                 Tasbeeh
             </h1>
-            <p className='text-7xl font-bold'>{tasbeeh}</p>
+            <p className='text-7xl font-bold'>{Tasbeeh}</p>
 
             <div>
-                <motion.button whileTap={{ scale: 0.9 }} whileHover={{ scale: 1.1 }} className='bg-violet-900 p-10 rounded-full mt-16' onClick={() => { setTasbeeh(tasbeeh + 1) }}><AiOutlinePlusCircle className="text-7xl text-gray-300" /></motion.button>
+                <motion.button whileTap={{ scale: 0.9 }} whileHover={{ scale: 1.1 }} className='bg-violet-900 p-10 rounded-full mt-16' onClick={() => { setTasbeeh(Tasbeeh + 1) }}><AiOutlinePlusCircle className="text-7xl text-gray-300" /></motion.button>
                 <br />
                 <motion.button whileTap={{ scale: 0.9 }} whileHover={{ scale: 1.1 }} className='mt-3 underline text-gray-200' onClick={() => setTasbeeh(0)}>reset</motion.button>
             </div>
